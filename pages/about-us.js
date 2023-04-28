1fc033f8de92215939447261a1b93f9193286dc3
Lucas <lucas.b.dev@proton.me>
2023-04-28 13:08:26 -0300

    braek

---
+++ pages/about-us.js
@@ -0,0 +1,14 @@
+const Page = () => {
+  return (
+    <>
+      <h1>About us Page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio nobis
+        ipsum maiores magnam voluptatem laborum perferendis ea ipsa
+        exercitationem? Sunt sed sapiente beatae quisquam eaque unde veritatis
+        reiciendis earum quae.
+      </p>
+    </>
+  );
+};
+export default Page;
---
+++ pages/events/index.js
@@ -0,0 +1,20 @@
+const Page = () => {
+  return (
+    <>
+      <h2>Events Page</h2>
+      <a href=''>
+        <img src='' alt='' />
+        <h2>Events in London</h2>
+      </a>
+      <a href=''>
+        <img src='' alt='' />
+        <h2>Events in San Francisco</h2>
+      </a>
+      <a href=''>
+        <img src='' alt='' />
+        <h2>Events in Barcelone</h2>
+      </a>
+    </>
+  );
+};
+export default Page;
